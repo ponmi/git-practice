1245c3f07ba83f02629ed2f8233fe5036084003a
ponmi <yosi0032@gmail.com>
2023-03-28 21:51:10 +0900

    rebase バグ修正ほげでの修正

--- NewClass.java
+++ NewClass.java
@@ -1,7 +1,8 @@
 public static void main(String args[]){
 	String hoge = "takashima";
 	System.print.out(hoge);
-	System.print.out("あほ");
+        System.print.out("あほ");
+
 	int number = 0;
 	// feature/バグ修正
 	if(number = 0){
@@ -15,4 +16,7 @@ public static void main(String args[]){
 	for(int i = 0; i < 5; i++){
 		System.print.out(i);
 	}
+	System.print.out("����");
+	//feature/バグ修正ほげ
+	System.print.out("ほげほげほげほげほげほげ");
 }
